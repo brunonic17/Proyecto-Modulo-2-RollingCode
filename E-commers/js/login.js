1f2b98145636bd6506fcb5bc1e61dd9c1e8b97ac
brunonic17 <bscistri@gmail.com>
2023-09-24 14:21:19 -0300

    errores de logica

---
+++ E-commers/js/login.js
@@ -0,0 +1,50 @@
+
+const usuariohtml=document.getElementById("exampleInputEmail1")
+const contraseñahtml=document.getElementById("exampleInputPassword1")
+const button_logingtml=document.getElementById("btnlogin")
+ 
+var login= new Object()
+let usuario=""
+let contraseña=""
+var Usuario="Usuario"
+
+usuariohtml.addEventListener("change",function cargausuario(e){ usuario=e.target.value},false
+)
+contraseñahtml.addEventListener("change",function cargacontraseña (e ){ contraseña=e.target.value
+},false
+ )
+button_logingtml.addEventListener ("click" , cargardatos(e),false)
+
+function cargardatos(e){
+    e.preventDefault();
+  e.stopPropagation();
+  login.usuario.push(usuario)
+  login.contraseña.push(contraseña)
+  localStorage.setItem(Usuario,JSON.stringify(login))
+}
+
+// class Login{
+//     constructor(Usuario,Contraseña){
+//       this._Usuario=Usuario
+//       this._Contraseña=Contraseña
+//     }
+
+//     set cargaUsuario(usuariohtml){
+//         this._Usuario =usuariohtml ;
+//     }
+
+//     set cargaContraseña(contraseñahtml){
+//       this._Contraseña=contraseñahtml;
+//     }
+
+//     get cargaLogin(){
+//         return console.log(this._Contraseña,this._Usuario)
+//     }
+// }
+
+
+// button_logingtml.addEventListener('click',function cargardatos(e){
+//     Login(usuariohtml,contraseñahtml)
+//     console.log(Login+e.taget.value)
+//     })
+
